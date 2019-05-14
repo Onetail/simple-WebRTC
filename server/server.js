0ac015fe915c083877b14824f7388605c4a3a27f
WayneChu <paosong91@gmail.com>
2019-05-15 00:39:39 +0800

    add server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const WebSocket = require("ws")
 const WebSocketServer = require('ws').Server
 const express = require("express")
 const app = express()
@@ -12,16 +13,17 @@ const http = require("http").createServer(app)
 const Global = require("./global/Global")
 
 var wss = new WebSocketServer({
-    port: Global.OutPort,
+    server: https
 })
 
-wss.broadcast = (data) => {
-    console.log(`\n\ndata = ` , data)
-    for(var i in this.clients) {
-        this.clients[i].send(data);
-    }
-};
 
+wss.broadcast = function (data) {
+    this.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
         console.log('received: %s', message);
@@ -43,3 +45,23 @@ https.listen(Global.Server.HTTPS_PORT, ()=>{
 http.listen(Global.Server.HTTP_PORT, ()=>{
     console.log(`Http Server listening in ${Global.Server.HTTP_PORT}..`)
 })
+
+function getIPAdress() {
+    var interfaces = require('os').networkInterfaces(),
+        alias,
+        iface;
+
+    for (let devName in interfaces) {　　　　
+        iface = interfaces[devName];　　　
+        for (var i = 0, max = iface.length ; i < max; i++) {
+            alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }　　
+    }
+    return false
+}
+
+var ip = getIPAdress();
+console.log("\n--------IP地址：" + ip+ "------\n");
