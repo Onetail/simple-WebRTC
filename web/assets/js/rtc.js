181623af16db8336fada39c1042637781bfd4319
WayneChu <paosong91@gmail.com>
2019-05-15 00:40:28 +0800

    update

--- web/assets/js/rtc.js
+++ web/assets/js/rtc.js
@@ -1,93 +1,101 @@
-const OutPort =  2019
+const OutPort = 2019
 var localVideo;
+var localStream;
 var remoteVideo;
 var peerConnection;
+var uuid;
+var serverConnection;
+
 var peerConnectionConfig = {
-    'iceServers': [
-        {'url': 'stun:stun.services.mozilla.com'}, 
-        {'url': 'stun:stun.l.google.com:19302'}
-    ]
+  'iceServers': [
+	{'urls': 'stun:stun.stunprotocol.org:3478'},
+	{'urls': 'stun:stun.l.google.com:19302'},
+  ]
 };
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
-window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
-
 function pageReady() {
-    localVideo = document.getElementById('localVideo');
-    remoteVideo = document.getElementById('remoteVideo');
-    serverConnection = new WebSocket(`wss://192.168.1.105:${OutPort}`);
-    serverConnection.onmessage = gotMessageFromServer;
+	uuid = createUUID();
 
-    var constraints = {
-        video: true,
-        audio: true,
-    };
+  	localVideo = document.getElementById('localVideo');
+  	remoteVideo = document.getElementById('remoteVideo');
 
-    if(navigator.getUserMedia) {
-        navigator.getUserMedia(constraints, getUserMediaSuccess, errorHandler);
-    } else {
-        alert('Your browser does not support getUserMedia API');
-    }
+	serverConnection = new WebSocket(`wss://192.168.31.231:${OutPort}`);
+	serverConnection.onmessage = gotMessageFromServer;
+
+  	var constraints = {
+		video: true,
+		audio: true,
+  	};
+
+  if(navigator.mediaDevices.getUserMedia) {
+	navigator.mediaDevices.getUserMedia(constraints).then(getUserMediaSuccess).catch(errorHandler);
+  } else {
+	alert('Your browser does not support getUserMedia API');
+  }
 }
 
 function getUserMediaSuccess(stream) {
-    localStream = stream;
-    if (window.URL) {
-        if ('srcObject' in localVideo) {
-            localVideo.srcObject = stream;
-          } else {
-            // Avoid using this in new browsers, as it is going away.
-            localVideo.src = URL.createObjectURL(stream);
-        }
-      } else {
-        localVideo.src = stream;
-    }
+  localStream = stream;
+  localVideo.srcObject = stream;
 }
 
 function start(isCaller) {
-    peerConnection = new RTCPeerConnection(peerConnectionConfig);
-    peerConnection.onicecandidate = gotIceCandidate;
-    peerConnection.onaddstream = gotRemoteStream;
-    peerConnection.addStream(localStream);
+  peerConnection = new RTCPeerConnection(peerConnectionConfig);
+  peerConnection.onicecandidate = gotIceCandidate;
+  peerConnection.ontrack = gotRemoteStream;
+  peerConnection.addStream(localStream);
 
-    if(isCaller) {
-        peerConnection.createOffer(gotDescription, errorHandler);
-    }
+  if(isCaller) {
+	peerConnection.createOffer().then(createdDescription).catch(errorHandler);
+  }
 }
 
 function gotMessageFromServer(message) {
-    if(!peerConnection) start(false);
+  if(!peerConnection) start(false);
 
-    var signal = JSON.parse(message.data);
-    if(signal.sdp) {
-        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function() {
-            peerConnection.createAnswer(gotDescription, errorHandler);
-        }, errorHandler);
-    } else if(signal.ice) {
-        peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice));
-    }
+  var signal = JSON.parse(message.data);
+
+  // Ignore messages from ourself
+  if(signal.uuid == uuid) return;
+
+  if(signal.sdp) {
+	peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
+	  // Only create answers in response to offers
+	  if(signal.sdp.type == 'offer') {
+		peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
+	  }
+	}).catch(errorHandler);
+  } else if(signal.ice) {
+	peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+  }
 }
 
 function gotIceCandidate(event) {
-    if(event.candidate != null) {
-        serverConnection.send(JSON.stringify({'ice': event.candidate}));
-    }
+  if(event.candidate != null) {
+	serverConnection.send(JSON.stringify({'ice': event.candidate, 'uuid': uuid}));
+  }
 }
 
-function gotDescription(description) {
-    console.log('got description');
-    peerConnection.setLocalDescription(description, function () {
-        serverConnection.send(JSON.stringify({'sdp': description}));
-    }, function() {console.log('set description error')});
+function createdDescription(description) {
+  console.log('got description');
+
+  peerConnection.setLocalDescription(description).then(function() {
+	serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
+  }).catch(errorHandler);
 }
 
 function gotRemoteStream(event) {
-    console.log('got remote stream');
-    remoteVideo.src = window.URL.createObjectURL(event.stream);
+  console.log('got remote stream');
+  remoteVideo.srcObject = event.streams[0];
 }
 
 function errorHandler(error) {
-    console.error(error);
+  console.log(error);
+}
+
+function createUUID() {
+  	function randomNumber() {
+		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  	}
+	return randomNumber() + randomNumber() + '-' + randomNumber() + '-' + randomNumber() + '-' + randomNumber() + '-' + randomNumber() + randomNumber() + randomNumber();
 }
